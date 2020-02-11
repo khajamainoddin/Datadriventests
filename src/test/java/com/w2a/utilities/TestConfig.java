3b5df8229cbb0c7d3a3c2b50c4ae7fc6175f7740
khajamainoddin <khajamainoddin.mohd@gmail.com>
2020-02-11 14:34:11 +0530

    datadriven testing

---
+++ src/test/java/com/w2a/utilities/TestConfig.java
@@ -0,0 +1,40 @@
+package com.w2a.utilities;
+public class TestConfig{
+
+
+	
+	public static String server="smtp.gmail.com";
+	public static String from = "khajamainoddin455@gmail.com";
+	public static String password = "Khaja1990@";
+	public static String[] to ={"khajamainoddin90@gmail.com","khajamainoddinror@gmail.com"};
+	public static String subject = "Extent Project Report";
+	
+	public static String messageBody ="TestMessage";
+	public static String attachmentPath="/home/khaja/Downloads/[Tutsgalaxy.com] - Automation Architect - Selenium WebDriver - 7 Live Projects/01 Live Project 1 - Data Driven Framework/DataDrivenFramework/target\r\n" + 
+			"2017_10_3_14_49_9.jpg";
+	public static String attachmentName="error.jpg";
+	
+	
+	
+	//SQL DATABASE DETAILS	
+	public static String driver="net.sourceforge.jtds.jdbc.Driver"; 
+	public static String dbConnectionUrl="jdbc:jtds:sqlserver://192.101.44.22;DatabaseName=monitor_eval"; 
+	public static String dbUserName="sa"; 
+	public static String dbPassword="$ql$!!1"; 
+	
+	
+	//MYSQL DATABASE DETAILS
+	public static String mysqldriver="com.mysql.jdbc.Driver";
+	public static String mysqluserName = "root";
+	public static String mysqlpassword = "selenium";
+	public static String mysqlurl = "jdbc:mysql://localhost:3306/acs";
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
